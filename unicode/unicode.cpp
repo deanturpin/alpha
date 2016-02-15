f308387568455d445ca813977f50a36929cdbe13
deanturpin <git@lvbtn.uk>
2016-02-15 17:36:38 +0000

    add basic unicode example

---
+++ unicode/unicode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "Unicode \u2764" << endl;
+	cout << "Unicode \u2765" << endl;
+	cout << "Unicode \u2766" << endl;
+	cout << "Unicode \u2767" << endl;
+	cout << "Unicode \u2768" << endl;
+
+	return 0;
+}
