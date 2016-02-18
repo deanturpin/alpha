69963df37b218de9c5c11677467e970e56e66bc6
deanturpin <git@lvbtn.uk>
2016-02-18 19:12:14 +0000

    make bitmap a class

--- unicode/bitmap.cpp
+++ unicode/bitmap.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <bitset>
 #include <string>
+
+namespace asc
+{
+	class bitmap
+	{
+		public:
+
+			bitmap(const unsigned int width, const unsigned int height)
+			{
+				using namespace std;
+
+				// Create bitmap
+				vector<vector<bool>> bitmap(height, vector<bool> (width, false));
+
+				// Top border
+				const string horizontal(width + 2, '-');
+				cout << horizontal << endl;
+
+				// Bitmap
+				for (auto r:bitmap)
+				{
+					cout << "|";
+
+					for (auto b:r)
+						cout << (b ? 'O' : ' ');
+
+					cout << "|" << endl;
+				}
+
+				// Bottom border
+				cout << horizontal << endl;
+			}
+	};
+}
 
 int main()
 {
-	using namespace std;
-
-	const int width = 100;
-	const int height = 40;
-
-	// Create bitmap
-	vector<vector<bool>> bitmap(height, vector<bool> (width, false));
-
-	// Top border
-	const string horizontal(width + 2, '-');
-	cout << horizontal << endl;
-
-	// Bitmap
-	for (auto r:bitmap)
-	{
-		cout << "|";
-
-		for (auto b:r)
-			cout << (b ? 'O' : ' ');
-
-		cout << "|" << endl;
-	}
-
-	// Bottom border
-	cout << horizontal << endl;
+	asc::bitmap b(140, 40);
 
 	return 0;
 }
