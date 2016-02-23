c0cbf094762d569865e401ef3ce148db73e54a39
deanturpin <git@lvbtn.uk>
2016-02-23 22:21:07 +0000

    move to a map representation

--- bitmap/bitmap.h
+++ bitmap/bitmap.h
@@ -1,16 +1,12 @@
 // Simple ASCII bitmap
 // Just include this header - all methods are inline
-// 
-// Example:
-//		asc::bitmap b(100, 50);
-//		b.set(10, 10);
-//		b.render();
 
 #ifndef ASC_BITMAP_H
 #define ASC_BITMAP_H
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 namespace asc
 {
@@ -20,28 +16,20 @@ namespace asc
 	{
 		public:
 
-			// Constructor
-			bitmap(const unsigned int w, const unsigned int h)
-				: width(w)
-				, height(h)
-				, bm(h, vector<bool>(w, false))
-			{
-			}
-
-			// Destructor
-			~bitmap()
-			{
-			}
+			// Constructor/destructor
+			bitmap() { ; }
+			~bitmap() { ; }
 
 			// Set a point in the bitmap
-			void set(const unsigned int x, const unsigned int y)
+			void set(const int x, const int y)
 			{
-				bm[min(y, height - 1)][min(x, width - 1)] = true;
+				_b[x].push_back(y);
 			}
 
 			// Render the bitmap
 			void render()
 			{
+				/*
 				cout << "^" << endl;
 				for (const auto r:bm)
 				{
@@ -55,19 +43,17 @@ namespace asc
 
 				// X axis
 				cout << "0" << string(width, '-') << ">" << endl;
-			}
+				*/
 
-			// Properties
-			const unsigned int width;
-			const unsigned int height;
+				for (const auto x:_b)
+					for (const auto y:x.second)
+						cout << x.first << ", " << y << endl;
+			}
 
 		private:
 
 			// The internal bitmap
-			vector<vector<bool>> bm;
-
-			// Default constructor not allowed
-			bitmap();
+			map<int, vector<int>> _b;
 	};
 }
 
--- bitmap/test_pattern.cpp
+++ bitmap/test_pattern.cpp
@@ -7,11 +7,17 @@ int main()
 	using namespace std;
 
 	// Create bitmap
-	asc::bitmap b(150, 50);
+	asc::bitmap b;
 
 	// Create test pattern
-	for (unsigned int i = 0; i < b.width; ++i)
-		b.set(i, i % b.height);
+	// for (unsigned int i = 0; i < b.width; ++i)
+		// b.set(i, i % b.height);
+
+	b.set(0, 0);
+	b.set(-10, 11);
+	b.set(-10, 22);
+	b.set(-10, -13);
+	b.set(-13, -4);
 
 	// Render it to screen
 	b.render();
