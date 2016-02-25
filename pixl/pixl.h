2ec488d29d8b0a77197ed7d0f4a91a9ed45d74f1
deanturpin <git@lvbtn.uk>
2016-02-25 21:55:24 +0000

    put the resizing into the app instead

--- pixl/pixl.h
+++ pixl/pixl.h
@@ -17,30 +17,22 @@ namespace mega
 		public:
 
 			// Constructor
-			pixl()
-				: x_scale(1)
-				, y_scale(1)
-					{ ; }
+			pixl() { ; }
 
 			// Destructor
 			~pixl() { ; }
 
-			// Set a point in the bitmap
+			// Set a point
 			void set(const int x, const int y)
 			{
-				// Scale parameters
-				const int _x = x / x_scale;
-				const int _y = y / y_scale;
-
 				// Check ranges
-				x_range.first = min(x_range.first, _x);
-				x_range.second = max(x_range.second, _x);
+				x_range.first = min(x_range.first, x);
+				x_range.second = max(x_range.second, x);
 
-				y_range.first = min(y_range.first, _y);
-				y_range.second = max(y_range.second, _y);
+				y_range.first = min(y_range.first, y);
+				y_range.second = max(y_range.second, y);
 
-				// Write to bitmap
-				bitmap[_y].push_back(_x);
+				points[y].push_back(x);
 			}
 
 			// Print some props
@@ -50,54 +42,99 @@ namespace mega
 				cout << "Y " << y_range.first << ", " << y_range.second << endl;
 			}
 
-			void scale(const int x, const int y)
-			{
-				x_scale = x;
-				y_scale = y;
-			}
-
-			// Render the bitmap
+			// Render the points
 			void render()
 			{
 				// Extract Y range
-				y_range.first = bitmap.cbegin()->first;
-				y_range.second = bitmap.crbegin()->first;
-
-				// const auto y_max = max(y_range.first, y_range.second);
+				y_range.first = points.cbegin()->first;
+				y_range.second = points.crbegin()->first;
 
 				string bar(x_range.second + 1, ' ');
 
-				// for (auto i = y_min; i <= y_max; ++i)
 				for (auto i = y_range.first; i <= y_range.second; ++i)
 				{
 					// Create an empty bar or an axis
 					bar.assign(bar.size(), (i == 0 ? '-' : ' '));
 
 					// Search for elements on current line
-					const auto element = bitmap.find(i);
+					const auto element = points.find(i);
 
-					if (element != bitmap.cend())
+					if (element != points.cend())
 						for (auto &e:element->second)
 							bar.at(e) = '*';
 
 					cout << "|" << bar << endl;
 				}
-
-				cout << y_range.second << endl;
 			}
+
+			/*
+			void dump()
+			{
+				for (const auto &line:points)
+					for (auto point:line.second)
+						cout << line.first << ", " << point << endl;
+
+				cout << "Size " << points.size() << endl;
+			}
+			 */
+
+			// Resize the point space
+			/*
+			void resize(const unsigned int x, const unsigned int y)
+			{
+				// Resize by running through the original map again
+				map<int, vector<int>> b;
+
+				const auto x_bins = x_range.second / x;
+				const auto y_bins = (y_range.second - y_range.first) / y;
+
+				for (const auto &line:points)
+				{
+					const int y_new = line.first;
+
+					for (auto point:line.second)
+					{
+						// const int x_new = line.first / x_bins;
+						// const int y_new = point / y_bins;
+
+						const int x_new = point;
+
+						// Check ranges
+						x_range.first = 0;
+						x_range.second = 0;
+						x_range.first = min(x_range.first, x_new);
+						x_range.second = max(x_range.second, x_new);
+
+						y_range.first = 0;
+						y_range.second = 0;
+						y_range.first = min(y_range.first, y_new);
+						y_range.second = max(y_range.second, y_new);
+
+						b[y_new].push_back(x_new);
+					}
+				}
+
+				points.clear();
+				points = b;
+
+						// cout << line.first << ", " << point << endl;
+
+				// cout << "X " << x_range.first << ", " << x_range.second << endl;
+				// cout << "Y " << y_range.first << ", " << y_range.second << endl;
+
+				// cout << "Resampled map size " << b.size() << endl;
+			}
+			*/
 
 		private:
 
-			// The internal bitmap
-			map<int, vector<int>> bitmap;
+			// Internal representation of the point space
+			// Y followed by a series of Xs
+			map<int, vector<int>> points;
 
 			// Ranges
 			pair<int, int> x_range;
 			pair<int, int> y_range;
-
-			// Scaling
-			int x_scale;
-			int y_scale;
 	};
 }
 
