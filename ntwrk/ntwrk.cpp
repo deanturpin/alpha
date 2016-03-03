991761ef59481e2c376f0df5d17c250780b1eead
deanturpin <git@lvbtn.uk>
2016-03-03 18:04:45 +0000

    extract only using getline

--- ntwrk/ntwrk.cpp
+++ ntwrk/ntwrk.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
-#include <regex>
+// #include <sstream>
+// #include <vector>
+// #include <regex>
 
 int main()
 {
 	using namespace std;
 
 	// Read complete file on stdin
-	stringstream raw;
-	raw << cin.rdbuf();
-	string host_file = raw.str();
+	// stringstream raw;
+	// raw << cin.rdbuf();
+	// string host_file = raw.str();
 
-	vector<string> ips;
+	// vector<string> ips;
 
 	// Extract all IPs
-	smatch match;
-	while (regex_search(host_file, match, regex("[0-9.]{7,}")))
-	{
-		ips.push_back(match[0]);
-		host_file = match.suffix().str();
-	}
+	// smatch match;
+	// while (regex_search(host_file, match, regex(".*[0-9.]{7,}.*")))
+	// {
+		// ips.push_back(match[0]);
+		// host_file = match.suffix().str();
+	// }
+
+	string line;
+	while (getline(cin, line))
+		cout << line << endl;
 
 	// Ping each IP
-	for (auto &ip:ips)
-	{
-		cout << ip << endl;
-		system(string("ping -w 1 " + ip).c_str());
-	}
+	// system(string("ping -w 1 " + ip).c_str());
+	// for (auto &ip:ips)
+		// cout << "\"" << ip << "\"" << endl;
 
 	return 0;
 }
