50899550e152130dfe4b916c246fafeb0ae92c0e
deanturpin <git@lvbtn.uk>
2016-03-07 19:48:08 +0000

    print all matches

--- ntwrk/ntwrk.cpp
+++ ntwrk/ntwrk.cpp
@@ -7,29 +7,18 @@ int main()
 {
 	using namespace std;
 
-	#ifdef GETLINE_VERSION
-	string line;
-	while (getline(cin, line))
-	{
-		string prefix("norm\t");
-		if (regex_search(line, regex("#")))
-			prefix = "comm\t";
-		else if (regex_search(line, regex("^$")))
-			prefix = "empt\t";
-
-		cout << prefix << line << endl;
-	}
-	#else
 	// Read complete file on stdin
 	stringstream raw;
 	raw << cin.rdbuf();
 	const string hosts = raw.str();
 
-	regex word_regex("(\\S+)");
+	regex word_regex("\\S+");
 	const auto words_begin = sregex_iterator(hosts.cbegin(), hosts.cend(), word_regex);
 
 	cout << "Found " << distance(words_begin, sregex_iterator()) << endl;
-	#endif
+
+	for (std::sregex_iterator i = words_begin; i != sregex_iterator(); ++i)
+		cout << i->str() << endl;
 
 	return 0;
 }
