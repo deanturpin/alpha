882f3d2d48f630982b5bf40813aaf0d5aa5dcf20
deanturpin <git@lvbtn.uk>
2016-03-09 11:50:12 +0000

    final keyword

--- c++/classes.cpp
+++ c++/classes.cpp
@@ -9,20 +9,38 @@ namespace tst
 		cout << "Classes" << endl;
 
 		// Explicit
-		struct A
 		{
-			A(int) { }
-		};
+			struct A
+			{
+				A(int) { }
+			};
 
-		struct B
-		{
-			explicit B(int) { }
-		};
+			struct B
+			{
+				explicit B(int) { }
+			};
 
-		A(1);
-		B(1);
+			A(1);
+			B(1);
+		}
 
 		// Final
+		{
+			struct A
+			{
+				virtual void foo() final {};
+				virtual void bar(){};
+			};
 
+			struct B final : public A
+			{
+				// Can't do this: A::foo is final
+				// void foo(){};
+				void bar(){};
+			};
+
+			// Can't do this: B is final
+			// struct C final : public B {};
+		}
 	}
 }
