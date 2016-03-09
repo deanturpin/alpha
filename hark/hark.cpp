0409c098df786bf2006eea1047e9fe590c56a9d7
deanturpin <git@lvbtn.uk>
2016-03-09 18:21:18 +0000

    simplify further

--- hark/hark.cpp
+++ hark/hark.cpp
@@ -15,14 +15,12 @@ int main()
 
 	cout << "Number of replies " << replies.size() << endl;
 
-	for (const auto &i:{1, 2, 3, 4})
-	{
-		// std::default_random_engine generator;
-		// std::uniform_int_distribution<int> distribution(0, replies.size() - 1);
-		// int i = distribution(generator);
+	for (const auto &r:replies)
+		cout << r << endl;
 
-		cout << i << "\t" << replies.at(static_cast<unsigned int>(1.0 * rand() * replies.size() / RAND_MAX)) << endl;
-	}
+	// std::default_random_engine generator;
+	// std::uniform_int_distribution<int> distribution(0, replies.size() - 1);
+	// int i = distribution(generator);
 
 	return 0;
 }
