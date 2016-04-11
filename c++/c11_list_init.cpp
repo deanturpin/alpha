3c497bf5fe7cfed056223aa6925cce008e6132a4
deanturpin <git@lvbtn.uk>
2016-04-11 21:03:13 +0100

    non member begin and end on a c array

--- c++/c11_list_init.cpp
+++ c++/c11_list_init.cpp
@@ -6,7 +6,7 @@ std::vector<int> ints()
 	return {1, 2, 3, 4, 5};
 }
 
-void list_init()
+void c11_list_init()
 {
 	using namespace std;
 
--- c++/c11_nonmember_begin_and_end.cpp
+++ c++/c11_nonmember_begin_and_end.cpp
@@ -3,4 +3,17 @@
 void c11_nonmember_begin_and_end()
 {
 	using namespace std;
+
+	// Create lambda to square a parameter
+	auto square = [](const int n) {return n * n;};
+
+	int array[] = {1, 2, 3};
+
+	// For each
+	for (const auto &a : array)
+		cout << square(a) << endl;
+
+	// Non-member	
+	for (auto i = begin(array); i != end(array); ++i)
+		cout << *i << endl;
 }
