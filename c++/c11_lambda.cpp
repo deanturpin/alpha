2b0f5a05cbf6a539f9d75f695ebc0d0c0284f01e
deanturpin <git@lvbtn.uk>
2016-04-11 20:26:54 +0100

    extend lambda

--- c++/c11_lambda.cpp
+++ c++/c11_lambda.cpp
@@ -4,20 +4,28 @@
 
 namespace tst
 {
-	void lambda()
+	// Lambda - anonymous functions
+	void c11_lambda()
 	{
 		using namespace std;
 
-		vector<int> vec {1, 2, 3, 4, 5, 6, 7};
+		vector<int> vec {1, 2, 3};
 
+		// Before
 		for (const auto &v:vec)
 			cout << v << endl;
 
 		cout << "for_each increment" << endl;
 
+		// Increment each element
 		for_each(vec.begin(), vec.end(), [](int &n){ ++n; });
 
+		// After
 		for (const auto &v:vec)
 			cout << v << endl;
+
+		auto square_and_print = [](int n) {cout << n * n << endl;};
+
+		square_and_print(5);
 	}
 }
