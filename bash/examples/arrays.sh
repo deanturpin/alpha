0a5355a68d2ff18027f1728d04566a1bb4635e8a
deanturpin <git@lvbtn.uk>
2016-04-29 13:41:15 +0100

    more arrays

--- bash/examples/arrays.sh
+++ bash/examples/arrays.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
-array=( one two three four five six )
+array=(
+one
+two
+three
+four
+)
 
 # Print each element
-for i in ${array[@]}; do echo $i; done
+echo ${array[@]}
+
+# Print indices
+echo ${!array[@]}
+
+# Clear an element
+unset -v array[2]
+echo ${array[*]}
+
+# Mapfile
+mapfile array2 < <(ls)
+echo lines ${#array2[*]}
+
+# Square brackets are a math context (no $)
+i=2
+echo ${array2[i + 1]}
+
+# Parameter expansion - turn all Os into Xs
+echo ${array[*]/o/x}
+
+set -- hello hola bonjour
+echo count _ $#
+echo param _ $*
+echo last  _ ${@:(-1)}
+
+set -- *.sh
+echo There are $# shell scripts
