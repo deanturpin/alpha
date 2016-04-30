bddbbb9f5a28b577919ce7b62db6293649415544
deanturpin <git@lvbtn.uk>
2016-04-30 13:24:45 +0100

    add binary converter

--- bash/examples/arithmetic.sh
+++ bash/examples/arithmetic.sh
@@ -3,3 +3,13 @@ echo $RANDOM
 
 # Leading zeros
 echo {000..009}
+
+# Base
+echo $(( 16#a8 ))
+echo $(( 2#11111111 + 2#1000 ))
+
+# Decimal to binary
+dec2bin=({0,1}{0,1}{0,1}{0,1}{0,1}{0,1}{0,1}{0,1})
+echo ${dec2bin[170]} 
+
+echo ${dec2bin[*]} 
