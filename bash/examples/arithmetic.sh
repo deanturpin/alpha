ba170d8b9803eb1e18867f557f406996c6d1ae6f
deanturpin <git@lvbtn.uk>
2016-05-04 09:01:16 +0100

    combine

--- bash/examples/arithmetic.sh
+++ bash/examples/arithmetic.sh
@@ -13,3 +13,17 @@ dec2bin=({0,1}{0,1}{0,1}{0,1}{0,1}{0,1}{0,1}{0,1})
 echo ${dec2bin[170]} 
 
 echo ${dec2bin[*]} 
+
+# Basic 
+echo $(( 1 * 2 + 3 / 4 ))
+
+# Default type is a string
+blah=1+2
+echo $blah
+
+# Declare as an int
+declare -i blah=3+4
+echo $blah
+
+blah+=5
+echo $blah
